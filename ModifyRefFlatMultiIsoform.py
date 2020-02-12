e2b51745cd028192adf066a683e0987877e22527
teisen <teisen@mit.edu>
2020-02-12 10:08:25 -0500

    bug fixed for handling of positive strand mRNAs.

--- ModifyRefFlatMultiIsoform.py
+++ ModifyRefFlatMultiIsoform.py
@@ -69,15 +69,15 @@ for line in RefFlat:
 			for exonIdx in range(len(exonStarts) - 1,0,-1): #the main code block for output exons
 				#Tuple of starts and ends.
 				currentExon = (int(exonStarts[exonIdx]), int(exonEnds[exonIdx]))
-				if txEndMod < currentExon[1]: #or =?
+				if currentExon[0] < txEndMod: #or =?
 					exonEndsMod[exonIdx] = str(txEndMod)
 					ListEnd = exonIdx
 					break
 				# else: 
 				# 	del exonStartsMod[exonIdx]
 				# 	del exonEndsMod[exonIdx]
-			exonStartsMod = exonStartsMod[:ListEnd]
-			exonEndsMod = exonEndsMod[:ListEnd]
+			exonStartsMod = exonStartsMod[:(ListEnd + 1)]
+			exonEndsMod = exonEndsMod[:(ListEnd + 1)]
 		
 			if cdsEnd > txEndMod:
 				cdsEnd = txEndMod
